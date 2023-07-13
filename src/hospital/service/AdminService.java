35deb42a7368989c68e1e5134ba9c85827b6e96f
Master <97798661+Love-YeLin@users.noreply.github.com>
2023-07-13 10:28:40 +0800

    Update AdminService.java

--- src/hospital/service/AdminService.java
+++ src/hospital/service/AdminService.java
@@ -45,4 +45,52 @@ public class AdminService {
         admin.setCreate_time(create_time);
         return adminDao.update(admin);
     }
+
+    public List<Hospital> AllHospital(){
+        try {
+            Connection connection = DbConnection.getConnection();
+            String sql = "select * from hospital ";
+            PreparedStatement pt = connection.prepareStatement(sql);
+            ResultSet rs = pt.executeQuery();
+            List<Hospital> hospitalList = new ArrayList<>();
+            while (rs.next()) {
+                Hospital hospital = new Hospital();
+                hospital.setId(rs.getInt("HospitalID"));
+                hospital.setName(rs.getString("HospitalName"));
+                hospital.setLevel(rs.getString("HospitalLevel"));
+                hospital.setAddress(rs.getString("HospitalAddress"));
+                hospital.setNumber(rs.getString("ContactNumber"));
+                hospital.setPresidentname(rs.getString("PresidentName"));
+                hospital.setTime(rs.getString("EstablishmentDate"));
+                hospital.setStatus(rs.getString("OperationStatus"));
+                hospital.setIntroduction(rs.getString("Introduction"));
+
+                hospitalList.add(hospital);
+            }
+            return hospitalList;
+        } catch (Exception var7) {
+            var7.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Department> AllDepartment(){
+        try {
+            Connection connection = DbConnection.getConnection();
+            String sql = "select * from department ";
+            PreparedStatement pt = connection.prepareStatement(sql);
+            ResultSet rs = pt.executeQuery();
+            List<Department> departmentList = new ArrayList<>();
+            while (rs.next()) {
+                Department department=new Department();
+                department.setDepartmentid(rs.getInt("departmentid"));
+                department.setDepartmentname(rs.getString("departmentname"));
+                departmentList.add(department);
+            }
+            return departmentList;
+        } catch (Exception var7) {
+            var7.printStackTrace();
+            return null;
+        }
+    }
 }
